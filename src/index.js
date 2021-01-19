90abd86c5973e2728b83e8deea516cc6c5d4192f
Dar <Levy>
2021-01-19 17:59:00 +0200

    Install & config Raven for sentry platform

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+import Raven from "raven-js";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
+
+Raven.config(
+  "https://fd77c953a1d04817955efe6460d4a96a@o507516.ingest.sentry.io/5598742"
+).install();
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
